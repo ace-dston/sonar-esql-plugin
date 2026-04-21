a3fc18138a6689c5383d26a2a0629d7a3aa04b4b
Daniel Stoner <dston@acehardware.com>
2026-04-21 14:44:01 -0500

    Remove unused comment lines tracking in MetricsVisitor

--- esql-frontend/src/main/java/com/exxeta/iss/sonar/esql/metrics/MetricsVisitor.java
+++ esql-frontend/src/main/java/com/exxeta/iss/sonar/esql/metrics/MetricsVisitor.java
@@ -113,14 +113,12 @@ public class MetricsVisitor extends SubscriptionVisitor {
 		saveMetricOnFile(CoreMetrics.NCLOC, lineVisitor.getLinesOfCodeNumber());
 
 		CommentLineVisitor commentVisitor = new CommentLineVisitor(context.getTopTree(), ignoreHeaderComments);
-		Set<Integer> commentLines = commentVisitor.getCommentLines();
 
 		saveMetricOnFile(CoreMetrics.COMMENT_LINES, commentVisitor.getCommentLineNumber());
 
 		FileLinesContext fileLinesContext = fileLinesContextFactory.createFor(this.inputFile);
 
 		linesOfCode.forEach(line -> fileLinesContext.setIntValue(CoreMetrics.NCLOC_DATA_KEY, line, 1));
-		commentLines.forEach(line -> fileLinesContext.setIntValue(CoreMetrics.COMMENT_LINES_DATA_KEY, line, 1));
 
 		Set<Integer> executableLines = new ExecutableLineVisitor(context.getTopTree()).getExecutableLines();
 		projectExecutableLines.put(inputFile, executableLines);
